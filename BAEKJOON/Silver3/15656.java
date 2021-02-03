0e66f52e8cefe1c653f4bf1e10997e2541279488
Kwon Yongjin <56419677+kokok22@users.noreply.github.com>
2021-02-03 15:16:57 +0900

    20210203

---
+++ BAEKJOON/Silver3/15656.java
@@ -0,0 +1,72 @@
+package com.ssafy.day01;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	static BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+	public static void main(String args[]) {
+		try {
+			String s = bf.readLine();
+		
+			StringTokenizer st = new StringTokenizer(s);
+			
+			int N = Integer.parseInt(st.nextToken());
+			int M = Integer.parseInt(st.nextToken());
+		
+			int[] arr = new int[N];
+		
+			s = bf.readLine();
+			st = new StringTokenizer(s);
+			
+			for(int i=0;i<N;i++)
+				arr[i] = Integer.parseInt(st.nextToken());
+			
+			Arrays.sort(arr);
+			
+			combination(arr, new int[M], 0);
+			bw.flush();
+			bw.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	private static void combination(int[] arr, int[] sel, int k) throws Exception{
+		if(k==sel.length) {
+			for(int i=0;i<sel.length;i++) {
+				bw.write(Integer.toString(sel[i])+" ");
+			}
+			bw.newLine();
+			return;
+		}
+		
+		for(int i=0;i<arr.length;i++) {
+				sel[k] = arr[i];
+				combination(arr,sel,k+1);
+		}
+	}
+}
+
+
+
+
+
+
+//combination(arr,new int[M],0,0);
+
+//private static void combination(int[] arr, int[] sel, int k, int idx) {
+//if(k==sel.length) {
+//	for(int i=0;i<sel.length;i++)
+//		System.out.print(sel[i]+" ");
+//	System.out.println();
+//	return;
+//}
+//
+//for(int i=idx;i<arr.length;i++) {
+//	sel[k] = arr[i];
+//	combination(arr,sel,k+1,i+1);
+//}
+//}
