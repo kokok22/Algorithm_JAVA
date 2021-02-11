2995c762f3075c79796480621e03c4ab8cbb3beb
Yongjin Kwon <56419677+kokok22@users.noreply.github.com>
2021-02-11 15:41:40 +0900

    20210211

---
+++ BAEKJOON/Silver5/NumberSorting3.java
@@ -0,0 +1,32 @@
+package com.ssafy.algorithm;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class NumberSorting3 {
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int N = Integer.parseInt(br.readLine());
+		int[] arr = new int[N];
+		
+		for(int i=0;i<N;i++)
+			arr[i] = Integer.parseInt(br.readLine());
+		
+		Arrays.sort(arr);
+		
+		for(int i=0;i<arr.length;i++)
+			bw.write(arr[i]+"\n");
+		
+		bw.flush();
+		br.close();
+		bw.close();
+	}
+}
