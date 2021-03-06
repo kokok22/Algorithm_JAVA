8169bfaa1b5ec47fec174282676b319fd1d69c50
Yongjin Kwon <56419677+kokok22@users.noreply.github.com>
2021-03-07 01:35:11 +0900

    20210307

---
+++ BAEKJOON/Silver4/17219.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.HashMap;
+import java.util.StringTokenizer;
+
+public class FindPassword {
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		
+		HashMap<String, String> hash = new HashMap<String, String>();
+		
+		for(int i=0;i<N;i++) {
+			st = new StringTokenizer(br.readLine());
+			String key = st.nextToken();
+			String value = st.nextToken();
+			hash.put(key, value);			
+		}
+		
+		for(int i=0;i<M;i++)
+			bw.write(hash.get(br.readLine())+"\n");
+		
+		bw.flush();
+		br.close();
+		bw.close();
+	}
+}
